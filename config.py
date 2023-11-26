6b1e245673216d322816755b63822c910a8c33fa
parthxsharma <151134722+parthxsharma@users.noreply.github.com>
2023-11-26 20:46:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/parthxsharma/Deepmusicbot",
+    "https://github.com/DAXXTEAM/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
